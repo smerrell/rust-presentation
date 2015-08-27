d31ae549bf49730375843dc0d52566bb88f791aa
Sam Merrell <merrell.sam@gmail.com>
2015-08-26 21:40:02 -0600

    Add code for ownership section

--- src/ownership.rs
+++ src/ownership.rs
@@ -1,7 +1,7 @@
 pub fn ownership() {
     let v = vec![1, 2, 3];
 
-    //let z = v; // transfer ownership of the vec to z
+    //let z = v; // transfer ownership of the Vector to z
 
     println!("v is {:?}", v); // can't use v anymore
 
@@ -10,8 +10,22 @@ pub fn ownership() {
     //take(s);
 
     println!("s is {:?}", s);
+
+    // The Copy trait (i.e. interface)
+    let mut a = 1;
+    let b = a; // primitives are all Copy
+
+    // a and b are both on the Stack
+
+    println!("a: {}, b: {}", a, b);
+
+    a = 99;
+    println!("a: {}, b: {}", a, b);
+
+    // Vectors do not implement Copy so they reference
+    // memory in the Heap
 }
 
 fn take<T>(v: Vec<T>) {
-    // consume the vec
+    // consume the Vector
 }
