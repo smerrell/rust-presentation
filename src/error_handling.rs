2107de8603a669aa251ec677e73abf9b73e6d792
Sam Merrell <merrell.sam@gmail.com>
2015-08-27 17:39:24 -0600

    Improve error handling code section

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -5,6 +5,7 @@ pub fn errors() {
     //     Some(T),
     //     None
     // }
+
     // enum Result<T, E> {
     //     Ok(T),
     //     Err(E),
@@ -15,6 +16,34 @@ pub fn errors() {
         Ok(a) => { println!("Parsed! {:?}", a); },
         Err(e) => { println!("Error! {:?}", e); },
     }
+}
+
+fn to_sip_address(value: &str) -> Result<Sip, ParseError> {
+    if value.is_empty() {
+        return Err(ParseError::EmptyString)
+    }
+
+    let re = Regex::new(r"^(?P<protocol>sip):(?P<user>[^@]+)@(?P<domain>[^:]+):?(?P<port>\d+)?$").unwrap();
+    if !re.is_match(&value) {
+        let err_message = format!("This string `{}` is not a valid SIP address", &value);
+        return Err(ParseError::InvalidFormat(err_message));
+    }
+
+    let captures = re.captures(&value).unwrap();
+
+    let mut port = None;
+    if let Some(p) = captures.name("port") {
+        port = Some(p.parse::<i32>().unwrap());
+    }
+
+    let sip = Sip {
+        protocol: captures.name("protocol").unwrap().to_string(),
+        user: captures.name("user").unwrap().to_string(),
+        domain: captures.name("domain").unwrap().to_string(),
+        port: port,
+    };
+
+    Ok(sip)
 }
 
 #[derive(Debug)]
@@ -28,32 +57,5 @@ struct Sip {
 #[derive(Debug)]
 enum ParseError {
     EmptyString,
-    InvalidFormat
-}
-
-fn to_sip_address(value: &str) -> Result<Sip, ParseError> {
-    if value.is_empty() {
-        return Err(ParseError::EmptyString)
-    }
-
-    let re = Regex::new(r"^(?P<protocol>sip):(?P<user>[^@]+)@(?P<domain>[^:]+):?(?P<port>\d+)?$").unwrap();
-    if !re.is_match(&value) {
-        return Err(ParseError::InvalidFormat);
-    }
-
-    let captures = re.captures(&value).unwrap();
-
-    let mut port = None;
-    if let Some(p) = captures.name("port") {
-        port = Some(p.parse::<i32>().unwrap());
-    }
-
-    let s = Sip {
-        protocol: captures.name("protocol").unwrap().to_string(),
-        user: captures.name("user").unwrap().to_string(),
-        domain: captures.name("domain").unwrap().to_string(),
-        port: port,
-    };
-
-    Ok(s)
+    InvalidFormat(String)
 }
